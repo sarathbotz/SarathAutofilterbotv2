9550e438740268af2fdb12b91f8c38aa3704c747
sarathbotz <91046239+sarathbotz@users.noreply.github.com>
2021-10-31 22:50:50 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -62,7 +62,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/sarathbotz">SarathAutofilterbotv2</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Sarathooq">SarathAutofilterbotv2</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
