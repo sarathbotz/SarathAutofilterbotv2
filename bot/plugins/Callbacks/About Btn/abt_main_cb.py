09405ff18b383c87b04d69e6bede96a8f80e76c6
sarathbotz <91046239+sarathbotz@users.noreply.github.com>
2021-11-01 20:27:51 +0530

    Update abt_main_cb.py

--- bot/plugins/Callbacks/About Btn/abt_main_cb.py
+++ bot/plugins/Callbacks/About Btn/abt_main_cb.py
@@ -39,7 +39,7 @@ async def cb_about(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/Sarathooq"
                 ),
                 
             InlineKeyboardButton
